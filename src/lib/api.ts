583541fc5d217f9e0ff1fe1ed732bd83e624072e
Cristian <smct.cristiansimbajon@gmail.com>
2025-11-26 13:45:16 +0800

    create api

---
+++ src/lib/api.ts
@@ -0,0 +1,37 @@
+import { CONFIG } from "../../config/config";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: CONFIG.API_URL,
+  withCredentials: true,
+  withXSRFToken: true,
+  headers: {
+    "Content-Type": undefined,
+    "X-Requested-With": "XMLHttpRequest",
+    Accept: "application/json",
+  },
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.code === "ECONNABORTED") {
+      console.error("Request timed out");
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+const sanctum = axios.create({
+  baseURL: CONFIG.API_URL,
+  withCredentials: true,
+  withXSRFToken: true,
+  headers: {
+    "Content-Type": undefined,
+    "X-Requested-With": "XMLHttpRequest",
+    Accept: "application/json",
+  },
+});
+
+export { api, sanctum };
